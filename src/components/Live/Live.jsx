01ca62fb1404fddbbedf458081f4090c72329fc5
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-05-20 21:40:21 +0530

    got some issue

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -2,13 +2,15 @@ import styles from "./Live.module.css";
 
 const Live = () => {
   return (
-    <div className={styles.live}>
-      <h3
-        className={`${styles.animate} ${styles.slideIn} ${styles.animateInfinite} ${styles.animateSlow}`}
-      >
-        Event: Oasis Bus Tour, JLN Stadium, Delhi * Collection Live
-      </h3>
-    </div>
+    <>
+      <div className={styles.live}>
+        <h3
+          className={`${styles.animate} ${styles.slideIn} ${styles.animateInfinite} ${styles.animateSlow}`}
+        >
+          Event: Oasis Bus Tour, JLN Stadium, Delhi * Collection Live
+        </h3>
+      </div>
+    </>
   );
 };
 
